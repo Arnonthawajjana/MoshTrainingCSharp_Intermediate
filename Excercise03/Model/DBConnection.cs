c73eae2776e38b22262ad64027d42138b827f643
Arnonthawajjana Chainarong <Arnonthawajjana@hotmail.com>
2017-06-06 18:02:26 +0700

    Add Innitail project for excercise about Interface and abstract

---
+++ Excercise03/Model/DBConnection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise03.Model
+{
+    abstract class DBConnection
+    {
+        private String _ConnectionString;
+
+        public string ConnectionString { get => _ConnectionString;}
+
+        protected DBConnection(String connectionString)
+        {
+        }
+
+        abstract public bool openConnection();
+
+        abstract public bool closeConnection();
+    }
+}
---
+++ Excercise03/Model/SqlConnection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise03.Model
+{
+    class SqlConnection : DBConnection
+    {
+        public SqlConnection(string connectionString) : base(connectionString)
+        {
+        }
+    }
+}
